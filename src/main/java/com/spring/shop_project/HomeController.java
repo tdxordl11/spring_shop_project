389e7c272c5db8efeca0757b2d8ef7c1e49399de
tdxordl11 <tdxordl11@naver.com>
2020-01-31 09:58:43 +0900

    초기 틀

--- src/main/java/com/spring/shop_project/HomeController.java
+++ src/main/java/com/spring/shop_project/HomeController.java
@@ -22,18 +22,16 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/logintest", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+
+		return "logintest";
+	}
+	
+	@RequestMapping(value = "/adminpage", method = RequestMethod.GET)
+	public String home2(Locale locale, Model model) {
+
+		return "adminpage";
 	}
 	
 }
